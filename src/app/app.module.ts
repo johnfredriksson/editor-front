34b5a480aa7f12f44d12ad39b984d879ad485562
John Fredriksson <jofr21@student.bth.se>
2022-09-01 16:51:11 +0200

    added quill

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { QuillModule } from 'ngx-quill';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    QuillModule
   ],
   providers: [],
   bootstrap: [AppComponent]
